39c36bd2e07d54b6d872fde82337b974534c57d9
Viktor Malych <victormalych@gmail.com>
2019-03-20 10:40:01 +0000

    Fixed integration test

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -76,11 +76,9 @@ public class App {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT employees.emp_no, first_name, last_name, departments.dept_name "
-                            + "FROM employees, dept_emp, departments "
-                            + "WHERE employees.emp_no = '" + ID + "' "
-                            + "AND dept_emp.emp_no = employees.emp_no "
-                            + "AND dept_emp.dept_no = departments.dept_no";
+                    "SELECT employees.emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE employees.emp_no = '" + ID + "' ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -90,7 +88,6 @@ public class App {
                 emp.emp_no = rset.getInt("employees.emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
-                emp.dept = getDepartment(rset.getString("departments.dept_name"));
                 return emp;
             } else
                 return null;
@@ -332,7 +329,7 @@ public class App {
             Statement stmt = con.createStatement();
             String strUpdate =
                     "INSERT INTO employees (emp_no, first_name, last_name, birth_date, gender, hire_date) " +
-                            "VALUES ('" + emp.emp_no + "', '" + emp.first_name + "', '" + emp.last_name + "', " +
+                            "VALUES (" + emp.emp_no + ", '" + emp.first_name + "', '" + emp.last_name + "', " +
                             "'9999-01-01', 'M', '9999-01-01')";
             stmt.execute(strUpdate);
         }
@@ -351,7 +348,7 @@ public class App {
         // Connect to database
         if (args.length < 1)
         {
-            a.connect("localhost:3306");
+            a.connect("localhost:33060");
         }
         else
         {
