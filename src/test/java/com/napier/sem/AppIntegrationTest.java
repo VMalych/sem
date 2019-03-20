677ce5c49aefbb9aba12b1d661b4a7634d83be69
Viktor Malych <victormalych@gmail.com>
2019-03-20 11:41:25 +0000

    Added github release

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -32,13 +32,13 @@ public class AppIntegrationTest
     void testAddEmployee()
     {
         Employee emp = new Employee();
-        emp.emp_no = 500000;
+        emp.emp_no = 500001;
         emp.first_name = "Kevin";
         emp.last_name = "Chalmers";
         app.addEmployee(emp);
-        emp = app.getEmployee(500000);
+        emp = app.getEmployee(500001);
         System.out.println(emp.first_name);
-        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.emp_no, 500001);
         assertEquals(emp.first_name, "Kevin");
         assertEquals(emp.last_name, "Chalmers");
     }
